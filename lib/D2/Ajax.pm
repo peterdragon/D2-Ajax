3c333b45c1f5eeb41c4eeccf3db473705d81d753
Peter Edwards <peter@dragonstaff.co.uk>
2016-10-18 16:06:38 +0100

    add v2

--- lib/D2/Ajax.pm
+++ lib/D2/Ajax.pm
@@ -12,4 +12,15 @@ get '/api/v1/greeting' => sub {
     return to_json { text => 'Hello World' };
 };
 
+get '/v1' => sub {
+    template 'v1';
+};
+
+get '/api/v2/greeting' => sub {
+	header 'Access-Control-Allow-Origin' => '*';
+    header 'Content-Type' => 'application/json';
+    return to_json { text => 'Hello World' };
+};
+
+
 true;
